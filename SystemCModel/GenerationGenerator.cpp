ee0da6d130a73931d072caf1161e99a9c1623ddf
Daniel Ejnar Larsen <201406535@post.au.dk>
2018-11-02 13:32:11 +0100

    added cpp file and comments on what i think is necessary

---
+++ SystemCModel/GenerationGenerator.cpp
@@ -0,0 +1,15 @@
+#include "GenerationGenerator.h"
+
+void GenerationGenerator::consumeRandom(void){
+
+}
+
+void GenerationGenerator::generateGeneration(void){
+  // get the generation_in
+
+  // crossover - need to set number of crossoverpoints?
+
+  // mutate - get random positions to mutate OR get a random number per bit and see if it mutates
+
+  // set generation_out
+}
